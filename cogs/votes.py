57fe7496e2cb15c6f200c8b256ba39cf337b063e
Alex Nørgaard <AlexNorgaard1170@gmail.com>
2019-12-14 14:44:41 +0000

    remove bad poll thingy, now I can do it better. ;)

--- cogs/votes.py
+++ cogs/votes.py
@@ -90,36 +90,6 @@ class Voting(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.send('Missing the question.')
 
-    @commands.command(hidden=True)
-    @commands.guild_only()
-    async def quickpoll(self, ctx, *questions_and_choices: str):
-        """Makes a poll quickly.
-        The first argument is the question and the rest are the choices.
-        """
-
-        if len(questions_and_choices) < 3:
-            return await ctx.send('Need at least 1 question with 2 choices.')
-        elif len(questions_and_choices) > 21:
-            return await ctx.send('You can only have up to 20 choices.')
-
-        perms = ctx.channel.permissions_for(ctx.me)
-        if not (perms.read_message_history or perms.add_reactions):
-            return await ctx.send('Need Read Message History and Add Reactions permissions.')
-
-        question = questions_and_choices[0]
-        choices = [(to_emoji(e), v)
-                   for e, v in enumerate(questions_and_choices[1:])]
-
-        try:
-            await ctx.message.delete()
-        except:
-            pass
-
-        body = "\n".join(f"{key}: {c}" for key, c in choices)
-        poll = await ctx.send(f'{ctx.author} asks: {question}\n\n{body}')
-        for emoji, _ in choices:
-            await poll.add_reaction(emoji)
-
 
 def setup(bot):
     """ Cog setup function. """
