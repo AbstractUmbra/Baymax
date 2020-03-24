275fa7402b85f26956f9c3b1645f46eebe5b3ee0
Alex Nørgaard <AlexNorgaard1170@gmail.com>
2020-03-24 21:33:15 +0000

    cleaned up!

--- cogs/twitch.py
+++ cogs/twitch.py
@@ -8,6 +8,9 @@ from discord.ext import commands, tasks
 class Twitch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.stream_endpoint = "https://api.twitch.tv/helix/streams"
+        self.user_endpoint = "https://api.twitch.tv/helix/users"
+        self.game_endpoint = "https://api.twitch.tv/helix/games"
         self._last_game = None
         self._last_td = datetime.datetime.utcnow()
         self.get_streamers.start()
@@ -19,7 +22,7 @@ class Twitch(commands.Cog):
         url = "https://api.twitch.tv/helix/streams"
         params = {"user_login": "speciaiisttv"}
 
-        async with self.bot.session.get(url, params=params, headers=self.headers) as resp:
+        async with self.bot.session.get(url, params=params, headers=self.bot.config.twitch_headers) as resp:
             jsony = await resp.json()
         await ctx.send(f"```json\n{jsony}```")
 
@@ -27,7 +30,7 @@ class Twitch(commands.Cog):
     async def get_streamers(self):
         """ [PROD] - Actual task. Runs nicely. """
         channel = self.bot.get_channel(690571015134380043)
-        async with self.bot.session.get("https://api.twitch.tv/helix/streams", params={"user_login": "speciaiisttv"}, headers=self.bot.config.twitch_headers) as resp:
+        async with self.bot.session.get(self.stream_endpoint, params={"user_login": "speciaiisttv"}, headers=self.bot.config.twitch_headers) as resp:
             stream_json = await resp.json()
         if stream_json['data'] != []:
             current_stream = datetime.datetime.utcnow() - self._last_td
@@ -37,10 +40,10 @@ class Twitch(commands.Cog):
             self._last_td = datetime.datetime.utcnow()
             embed = discord.Embed(
                 title=f"Specialist is live with: {stream_json['data'][0]['title']}", colour=discord.Colour.blurple(), url="https://twitch.tv/speciaiisttv")
-            async with self.bot.session.get("https://api.twitch.tv/helix/games", params={
+            async with self.bot.session.get(self.game_endpoint, params={
                     "id": f"{stream_json['data'][0]['game_id']}"}, headers=self.bot.config.twitch_headers) as game_resp:
                 game_json = await game_resp.json()
-            async with self.bot.session.get("https://api.twitch.tv/helix/users", params={"id": stream_json['data'][0]['user_id']}, headers=self.bot.config.twitch_headers) as user_resp:
+            async with self.bot.session.get(self.user_endpoint, params={"id": stream_json['data'][0]['user_id']}, headers=self.bot.config.twitch_headers) as user_resp:
                 user_json = await user_resp.json()
             embed.set_author(name=stream_json['data'][0]['user_name'],
                              icon_url=f"{user_json['data'][0]['profile_image_url']}")
