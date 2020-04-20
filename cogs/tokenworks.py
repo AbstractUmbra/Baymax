1330f460c5c9669cb96f71981434a44f6a779079
root <root@3c6514bb27ef>
2020-04-20 15:35:07 +0100

    Cleanup

--- cogs/tokenworks.py
+++ cogs/tokenworks.py
@@ -1,34 +1,42 @@
 """ Token parser """
 import asyncio
 import base64
-from typing import Union
 
 import discord
 from discord.ext import commands
 
+from utils import checks
+
 
 class TokenWorks(commands.Cog):
+    """ This is for when silly people reveal their token. """
+
     def __init__(self, bot):
         self.bot = bot
 
     async def decode_tok(self, token: str) -> int:
+        """ base 64 decode handler. """
         token_id = int(base64.b64decode(token))
         return token_id
 
-    @commands.group(hidden=True, invoke_without_command=True)
+    @commands.group()
+    @checks.has_guild_permissions(manage_members=True)
     async def token(self, ctx):
+        """ Token primary command. """
         if not ctx.invoked_subcommand:
             return
 
     @token.command(name="i")
     async def token_info(self, ctx, *, _token: str):
-        decoded_token = await self.decode_tok(_token.split(".")[0])
+        """ Use the token to get the information on the bot. """
+        try:
+            decoded_token = await self.decode_tok(_token.split(".")[0])
+        except ValueError:
+            return await ctx.send("This can't be decoded. Is it a valid token?")
         token_user = await self.bot.fetch_user(decoded_token)
         if not token_user:
             return await ctx.send("User ID doesn't work, sadly.")
         time = token_user.created_at.strftime("%d-%m-%Y %H:%M:%S")
-        client_info = InfoClient(loop=asyncio.get_event_loop())
-        await client_info.start(_token)
         embed = discord.Embed(
             description=f"""
                             **Username**: `{token_user.name}`
@@ -36,53 +44,64 @@ class TokenWorks(commands.Cog):
                             **Existed since**: `{time}`
                             **Bot**: `{token_user.bot}`
                         """)
+        embed.set_thumbnail(url=token_user.avatar_url)
+        client_info = InfoClient(loop=asyncio.get_event_loop())
+        try:
+            await client_info.start(_token)
+        except discord.LoginFailure:
+            await ctx.send(embed=embed)
+            return token_user
         embed.add_field(name="Owner", value=f"{client_info.owner}")
         if client_info.team:
             team_list = [str(member) for member in client_info.team]
             embed.add_field(name="Owner", value="\n".join(team_list))
-        embed.set_thumbnail(url=token_user.avatar_url)
         await ctx.send(embed=embed)
+        return token_user
 
     @token.command(name="w")
     async def token_warn(self, ctx, *, token: str):
+        """ Token boy. This will warn the author of the leaked token. """
         try:
             b64_tok = token.split(".")[0]
-        except Exception as err:  # TODO Find exception
+        except Exception as err:
             return await ctx.send(f"Not a valid token in here.\n{err}")
         try:
-            token_user_id = int(base64.b64decode(b64_tok))
-        except Exception as err:  # TODO Find exception
+            int(base64.b64decode(b64_tok))
+        except Exception as err:
             return await ctx.send(f"Couldn't get the token decoded. Is it valid?\n{err}")
-        await self.token_info(ctx, _token=token_user_id)
+        client_info = await self.token_info(ctx, _token=token)
         warns = SpamClient(loop=asyncio.get_event_loop())
-        await warns.start(token)
+        if client_info.bot:
+            await warns.start(token)
+        else:
+            await warns.start(token, bot=False)
         await ctx.send(f"Warned {warns.messages_sent} channels.")
 
-    @token_warn.error
-    async def tk_error(self, ctx, error):
+    async def cog_command_error(self, ctx, error):
+        """ Cog error handler. """
         if hasattr(error, "original"):
             error = error.original
         if isinstance(error, discord.errors.LoginFailure):
-            await ctx.send("Nice. Not a valid token.")
+            return await ctx.send("Nice. Not a valid token.")
+        elif isinstance(error, commands.CheckFailure):
+            return await ctx.send(f"Sorry to use `{ctx.command.name}` you need to be the owner of have **Manage Members**.")
+        else:
+            return await ctx.send(f"```{type(error)}\n{error}```")
 
-    @token_info.error
-    async def tk_info_error(self, ctx, error):
-        if hasattr(error, "original"):
-            error = error.original
-        if isinstance(error, discord.errors.LoginFailure):
-            await ctx.send("Nice. Not a valid token.")
 
 
 class SpamClient(discord.Client):
+    """ Quick client to handle the token stuff. """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.messages_sent = 0
 
     async def on_ready(self):
-        # channel stuff
+        """ On ready for warn. """
         for channel in self.get_all_channels():
             try:
-                await channel.send("CHANGE YOUR FUCKING TOKEN.")
+                await channel.send("Oh look, you've leaked your token and now I can do this. Please go and change it.")
             except Exception as err:
                 print(err)
             else:
@@ -91,12 +110,15 @@ class SpamClient(discord.Client):
 
 
 class InfoClient(discord.Client):
+    """ Info gather client. """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.owner = None
         self.team = None
 
     async def on_ready(self):
+        """ On ready for info. """
         _app = await self.application_info()
         self.owner = _app.owner
         if _app.team:
