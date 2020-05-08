d1dd1be70c163ea750b3554c24fe092ede2469ad
Revan <AlexNorgaard1170@gmail.com>
2020-05-08 22:47:11 +0100

    Now proper error handling! :D

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -80,42 +80,61 @@ class Reddit(commands.Cog):
     def _gen_embeds(self, requester: str, iterable: list, nsfw_channel: bool) -> typing.List[discord.Embed]:
         """ Generate many embeds from the top 10 posts on each subreddit. """
         embeds = []
+
         for item in iterable:
             if item.nsfw and not nsfw_channel:
                 continue
+
             embed = discord.Embed(
                 title=item.title,
                 description=item.self_text,
                 colour=discord.Colour.red(),
                 url=item.url)
+
             embed.set_author(name=item.author)
+
             if item.image_link:
                 embed.set_image(url=item.image_link)
+
             if item.video_link:
                 embed.add_field(
                     name="Video", value=f"[Click me!]({item.video_link})", inline=False)
+
             embed.add_field(name="Upvotes", value=item.upvotes, inline=True)
             embed.add_field(name="Total comments", value=item.comment_count)
             fmt = f"Result {iterable.index(item)}/{len(iterable)-1}"
             embed.set_footer(
                 text=f"{fmt} | {item.subreddit} | Requested by: {requester}")
+
             embeds.append(embed)
+
         return embeds
 
-    async def _perform_search(self, requester: str, nsfw_approved: bool, subreddit: str, sort_by: str):
+    async def _perform_search(self, requester: str, channel: discord.TextChannel, subreddit: str, sort_by: str):
         """ Performs the search for queries with aiohttp. Returns 10 items. """
+        async with self.bot.session.get(
+                f"https://reddit.com/r/{subreddit}/about.json", headers=self.headers) as subr_top_resp:
+            subr_deets = await subr_top_resp.json()
+
+        if subr_deets['data'].get('over18', None) and not channel.is_nsfw():
+            raise commands.NSFWChannelRequired(channel)
+
         async with self.bot.session.get(
                 f"https://reddit.com/r/{subreddit}/{sort_by}.json", headers=self.headers) as subr_resp:
             subreddit_json = await subr_resp.json()
+
         subreddit_pages = []
         common_img_exts = (".jpg", ".jpeg", ".png", ".gif", ".webm")
+
         idx = 0
         for post_data in subreddit_json['data']['children']:
             image_url = None
             video_url = None
             self_text = None
+
             if idx == 20:
                 break
+
             _short = post_data['data']
             if _short['stickied']:
                 idx += 1
@@ -137,13 +156,16 @@ class Reddit(commands.Cog):
             subreddit_pages.append(SubredditPost(
                 url, subreddit, title, upvotes, nsfw=nsfw, image_link=image_url, video_link=video_url, self_text=self_text, comment_count=comments, author=author))
             idx += 1
-        return self._gen_embeds(requester, subreddit_pages, nsfw_approved)
+
+        return self._gen_embeds(requester, subreddit_pages, channel.is_nsfw())
 
     @commands.command(name="reddit")
     @commands.cooldown(5, 300, commands.BucketType.user)
     async def _reddit(self, ctx: commands.Context, subreddit: str, sort_by: str = "hot"):
         """ Main Reddit command, subcommands to be added. """
-        embeds = await self._perform_search(str(ctx.author), ctx.channel.is_nsfw(), subreddit, sort_by)
+        embeds = await self._perform_search(str(ctx.author), ctx.channel, subreddit, sort_by)
+        if not embeds:
+            raise commands.BadArgument("Bad subreddit.", subreddit)
         pages = menus.MenuPages(source=SubredditPageSource(
             range(1, 10), embeds), clear_reactions_after=True)
         await pages.start(ctx)
@@ -151,11 +173,11 @@ class Reddit(commands.Cog):
     @_reddit.error
     async def reddit_error(self, ctx, error):
         error = getattr(error, "original", error)
-        if isinstance(error, IndexError):
-            error.handled = True
+        if isinstance(error, commands.NSFWChannelRequired):
+            return await ctx.send("This ain't an NSFW channel.")
+        elif isinstance(error, commands.BadArgument):
             msg = ("There seems to be no Reddit posts to show, common cases are:\n"
-                   "- Not a real subreddit.\n"
-                   "- You're requesting NSFW posts in a non-nsfw channel.")
+                   "- Not a real subreddit.\n")
             return await ctx.send(msg)
 
 
