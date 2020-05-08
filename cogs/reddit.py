e79634a28559671e367d3b21d3dc52496b8285a8
Revan <AlexNorgaard1170@gmail.com>
2020-05-08 23:31:52 +0100

    quick edit to remove '/r/' from subreddit entries

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -163,6 +163,7 @@ class Reddit(commands.Cog):
     @commands.cooldown(5, 300, commands.BucketType.user)
     async def _reddit(self, ctx: commands.Context, subreddit: str, sort_by: str = "hot"):
         """ Main Reddit command, subcommands to be added. """
+        subreddit = subreddit.strip("/r/")
         embeds = await self._perform_search(str(ctx.author), ctx.channel, subreddit, sort_by)
         if not embeds:
             raise commands.BadArgument("Bad subreddit.", subreddit)
