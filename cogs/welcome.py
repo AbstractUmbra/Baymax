a05cd1eb14d60f4ed0ecd55e49f8c08cb1648151
Revan <AlexNorgaard1170@gmail.com>
2020-06-06 17:40:47 +0100

    welcome query

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -25,7 +25,7 @@ class Welcome(commands.Cog):
         channel = member.guild.get_channel(results['welcome_channel'])
         return await channel.send(f"Hey {member.mention}\n\n{results['welcome_message']}")
 
-    @commands.group(name="welcome")
+    @commands.group(name="welcome", invoke_without_command=True)
     async def welcome_group(self, ctx, channel: discord.TextChannel, *, message: str):
         """ Group. If no subcommand then create a welcome message. """
         if ctx.invoked_subcommand:
@@ -45,6 +45,16 @@ class Welcome(commands.Cog):
         query = "DELETE FROM welcome_config WHERE guild_id = $1"
         await self.bot.pool.execute(query, ctx.guild.id)
         return await ctx.message.add_reaction("<:TickYes:672157420574736386>")
+
+    @welcome_group.command(name="query")
+    async def welcome_query(self, ctx):
+        """ Command. Let's have a look at your active help message. """
+        query = "SELECT * FROM welcome_config WHERE guild_id = $1"
+        record = await self.bot.pool.fetchrow(query, ctx.guild.id)
+        channel = ctx.guild.get_channel(record['welcome_channel'])
+        if not channel:
+            return await ctx.send("It seems the welcome channel has been deleted.")
+        return await ctx.send(f"Message is being sent to {channel.mention}. Message is:\n\n{record['welcome_message']}")
 
     @commands.command()
     async def iexist(self, ctx):
