f921b532a6f2632c6c558d783a2c26f36fb9aa8e
AbstractUmbra <Umbra@AbstractUmbra.xyz>
2020-08-03 22:55:56 +0100

    new event types

--- cogs/specialist.py
+++ cogs/specialist.py
@@ -41,8 +41,17 @@ class Specialist(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.image_urls = {
+            "dow3": "https://i.imgur.com/kYGODxo.jpg",
+            "swtor": "https://i.imgur.com/0wjY56D.png",
+            "gtfo": "https://www.mkaugaming.com/wp-content/uploads/2020/03/2020-03-19_00078.jpg",
+            "bfme2": "https://download.hipwallpaper.com/desktop/1920/1080/66/33/HBOjoz.jpg",
+            "aoe2": "https://news.xbox.com/en-us/wp-content/uploads/sites/2/HERO-35.jpg?fit=1920%2C1080",
+            "bf2": "https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2020/02/featured-ajankloss-action.jpg.adapt.crop191x100.628p.jpg",
+            "coh2": "https://hb.imgix.net/3d9954a268d7c9e03a5715e7a3d58a53ba823274.jpg?auto=compress,format&fit=crop&h=353&w=616&s=fc9540e6803dba1bb5fa1d14fafeacfa",
+            "ih": "https://i.ytimg.com/vi/KRkrF4TUHmQ/maxresdefault.jpg"}
 
-    async def cog_check(self, ctx: commands.Context) -> bool:
+    def cog_check(self, ctx: commands.Context) -> bool:
         if ctx.guild:
             return ctx.guild.id == 690566307409821697
         return False
@@ -135,7 +144,7 @@ class Specialist(commands.Cog):
                          icon_url=ctx.guild.owner.avatar_url)
         embed.set_thumbnail(url=ctx.author.avatar_url)
         embed.set_image(
-            url="https://download.hipwallpaper.com/desktop/1920/1080/66/33/HBOjoz.jpg")
+            url=self.image_urls['bfme2'])
         embed.add_field(name="Plan of action",
                         value=f"{when.arg}", inline=False)
         embed.add_field(
@@ -179,7 +188,7 @@ class Specialist(commands.Cog):
         embed = discord.Embed(title="AOE2 Event Prep!",
                               colour=discord.Colour.red())
         embed.set_image(
-            url="https://news.xbox.com/en-us/wp-content/uploads/sites/2/HERO-35.jpg?fit=1920%2C1080")
+            url=self.image_urls['aoe2'])
         embed.set_author(name=ctx.guild.owner.name,
                          icon_url=ctx.guild.owner.avatar_url)
         embed.add_field(name="Plan of action", value=f"{when.arg}")
@@ -220,7 +229,7 @@ class Specialist(commands.Cog):
         embed = discord.Embed(title="BF2 Event Prep!",
                               colour=discord.Colour.red())
         embed.set_image(
-            url="https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2020/02/featured-ajankloss-action.jpg.adapt.crop191x100.628p.jpg")
+            url=self.image_urls['bf2'])
         embed.set_author(name=ctx.guild.owner.name,
                          icon_url=ctx.guild.owner.avatar_url)
         embed.add_field(name="Plan of action", value=f"{when.arg}")
@@ -261,7 +270,7 @@ class Specialist(commands.Cog):
         embed = discord.Embed(title="COH2 Event Prep!",
                               colour=discord.Colour.red())
         embed.set_image(
-            url="https://hb.imgix.net/3d9954a268d7c9e03a5715e7a3d58a53ba823274.jpg?auto=compress,format&fit=crop&h=353&w=616&s=fc9540e6803dba1bb5fa1d14fafeacfa")
+            url=self.image_urls['coh2'])
         embed.set_author(name=ctx.guild.owner.name,
                          icon_url=ctx.guild.owner.avatar_url)
         embed.add_field(name="Plan of action", value=f"{when.arg}")
@@ -302,7 +311,7 @@ class Specialist(commands.Cog):
         embed = discord.Embed(title="SWTOR Event Prep!",
                               colour=discord.Colour.red())
         embed.set_image(
-            url="https://i.imgur.com/0wjY56D.png")
+            url=self.image_urls['swtor'])
         embed.set_author(name=ctx.guild.owner.name,
                          icon_url=ctx.guild.owner.avatar_url)
         embed.add_field(name="Plan of action", value=f"{when.arg}")
@@ -343,7 +352,89 @@ class Specialist(commands.Cog):
         embed = discord.Embed(title="GTFO Event Prep!",
                               colour=discord.Colour.red())
         embed.set_image(
-            url="https://www.mkaugaming.com/wp-content/uploads/2020/03/2020-03-19_00078.jpg")
+            url=self.image_urls['gtfo'])
+        embed.set_author(name=ctx.guild.owner.name,
+                         icon_url=ctx.guild.owner.avatar_url)
+        embed.add_field(name="Plan of action", value=f"{when.arg}")
+        embed.add_field(
+            name="When", value=f"{(when.dt + datetime.timedelta(hours=1)).strftime('%d-%m-%Y %H:%M')}")
+        embed.description = "Add a reaction to this post to join this event!"
+        await message.edit(content="", embed=embed)
+        return await ctx.message.delete()
+
+    @event.command(aliases=['DOW3'], usage="<when>", invoke_without_command=True)
+    async def dow3(self, ctx: commands.Context, *, when: time.UserFriendlyTime(commands.clean_content, default="\u2026")):
+        """ Create a GTFO event. """
+        reminder = self.bot.get_cog("Reminder")
+        if not reminder:
+            return await ctx.send("Sorry, this functionality is currently unavailable.")
+        message = await ctx.send("Placeholder")
+        when.dt = when.dt - datetime.timedelta(hours=1)
+        event_trigger = when.dt - datetime.timedelta(minutes=15)
+        cancellation_trigger = when.dt - datetime.timedelta(hours=2)
+        role_str = "".join(random.choice(
+            string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(5))
+        role = await ctx.guild.create_role(name=f"DOW3-{role_str}", colour=discord.Colour(0xcbe029),
+                                           reason="Event creation.", mentionable=False)
+        await reminder.create_timer(event_trigger, 'dow3', ctx.author.id,
+                                    ctx.channel.id,
+                                    when.arg,
+                                    connection=ctx.db,
+                                    created=ctx.message.created_at,
+                                    message_id=message.id,
+                                    role_id=role.id)
+        await reminder.create_timer(cancellation_trigger, 'event_check', ctx.author.id,
+                                    ctx.channel.id,
+                                    when.arg,
+                                    connection=ctx.db,
+                                    created=ctx.message.created_at,
+                                    message_id=message.id,
+                                    role_id=role.id)
+        embed = discord.Embed(title="DOW3 Event Prep!",
+                              colour=discord.Colour.red())
+        embed.set_image(
+            url=self.image_urls['dow3'])
+        embed.set_author(name=ctx.guild.owner.name,
+                         icon_url=ctx.guild.owner.avatar_url)
+        embed.add_field(name="Plan of action", value=f"{when.arg}")
+        embed.add_field(
+            name="When", value=f"{(when.dt + datetime.timedelta(hours=1)).strftime('%d-%m-%Y %H:%M')}")
+        embed.description = "Add a reaction to this post to join this event!"
+        await message.edit(content="", embed=embed)
+        return await ctx.message.delete()
+
+    @event.command(aliases=['ih', 'IH', 'ironharvest'], usage="<when>", invoke_without_command=True)
+    async def iron_harvest(self, ctx: commands.Context, *, when: time.UserFriendlyTime(commands.clean_content, default="\u2026")):
+        """ Create an Iron Harvest event. """
+        reminder = self.bot.get_cog("Reminder")
+        if not reminder:
+            return await ctx.send("Sorry, this functionality is currently unavailable.")
+        message = await ctx.send("Placeholder")
+        when.dt = when.dt - datetime.timedelta(hours=1)
+        event_trigger = when.dt - datetime.timedelta(minutes=15)
+        cancellation_trigger = when.dt - datetime.timedelta(hours=2)
+        role_str = "".join(random.choice(
+            string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(5))
+        role = await ctx.guild.create_role(name=f"IH-{role_str}", colour=discord.Colour(0xcbe029),
+                                           reason="Event creation.", mentionable=False)
+        await reminder.create_timer(event_trigger, 'ih', ctx.author.id,
+                                    ctx.channel.id,
+                                    when.arg,
+                                    connection=ctx.db,
+                                    created=ctx.message.created_at,
+                                    message_id=message.id,
+                                    role_id=role.id)
+        await reminder.create_timer(cancellation_trigger, 'event_check', ctx.author.id,
+                                    ctx.channel.id,
+                                    when.arg,
+                                    connection=ctx.db,
+                                    created=ctx.message.created_at,
+                                    message_id=message.id,
+                                    role_id=role.id)
+        embed = discord.Embed(title="Iron Harvest Event Prep!",
+                              colour=discord.Colour.red())
+        embed.set_image(
+            url=self.image_urls['ih'])
         embed.set_author(name=ctx.guild.owner.name,
                          icon_url=ctx.guild.owner.avatar_url)
         embed.add_field(name="Plan of action", value=f"{when.arg}")
@@ -364,6 +455,8 @@ class Specialist(commands.Cog):
                    OR event = 'coh2'
                    OR event = 'swtor'
                    OR event = 'gtfo'
+                   OR event = 'dow3'
+                   OR event = 'ih'
                    AND extra #>> '{args,0}' = $1
                    ORDER BY expires
                    LIMIT 10;
@@ -460,7 +553,7 @@ class Specialist(commands.Cog):
         embed.set_author(name=event_author.display_name,
                          icon_url=event_author.avatar_url)
         embed.set_image(
-            url="https://download.hipwallpaper.com/desktop/1920/1080/66/33/HBOjoz.jpg")
+            url=self.image_urls['bfme2'])
         if member_names:
             embed.add_field(name="Members that signed up",
                             value=", ".join(member_names))
@@ -495,7 +588,7 @@ class Specialist(commands.Cog):
         embed.set_author(name=event_author.display_name,
                          icon_url=event_author.avatar_url)
         embed.set_image(
-            url="https://news.xbox.com/en-us/wp-content/uploads/sites/2/HERO-35.jpg?fit=1920%2C1080")
+            url=self.image_urls['aoe2'])
         if member_names:
             embed.add_field(name="Members that signed up",
                             value=", ".join(member_names))
@@ -530,7 +623,7 @@ class Specialist(commands.Cog):
         embed.set_author(name=event_author.display_name,
                          icon_url=event_author.avatar_url)
         embed.set_image(
-            url="https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2020/02/featured-ajankloss-action.jpg.adapt.crop191x100.628p.jpg")
+            url=self.image_urls['bf2'])
         if member_names:
             embed.add_field(name="Members that signed up",
                             value=", ".join(member_names))
@@ -565,7 +658,7 @@ class Specialist(commands.Cog):
         embed.set_author(name=event_author.display_name,
                          icon_url=event_author.avatar_url)
         embed.set_image(
-            url="https://hb.imgix.net/3d9954a268d7c9e03a5715e7a3d58a53ba823274.jpg?auto=compress,format&fit=crop&h=353&w=616&s=fc9540e6803dba1bb5fa1d14fafeacfa")
+            url=self.image_urls['coh2'])
         if member_names:
             embed.add_field(name="Members that signed up",
                             value=", ".join(member_names))
@@ -600,7 +693,7 @@ class Specialist(commands.Cog):
         embed.set_author(name=event_author.display_name,
                          icon_url=event_author.avatar_url)
         embed.set_image(
-            url="https://i.imgur.com/0wjY56D.png")
+            url=self.image_urls['swtor'])
         if member_names:
             embed.add_field(name="Members that signed up",
                             value=", ".join(member_names))
@@ -648,6 +741,74 @@ class Specialist(commands.Cog):
         return await current_message.delete(delay=900)
 
     @commands.Cog.listener()
+    async def on_dow3_timer_complete(self, event):
+        """ On 'dow3' event timer complete. """
+        author_id, channel_id, message = event.args
+
+        try:
+            channel: discord.TextChannel = self.bot.get_channel(channel_id) or (await self.bot.fetch_channel(channel_id))
+        except discord.HTTPException:
+            return
+
+        message_id: int = event.kwargs.get('message_id')
+        prev_message: discord.Message = await channel.fetch_message(message_id)
+        role: discord.Role = channel.guild.get_role(event.kwargs.get('role_id'))
+        await role.edit(mentionable=True)
+        reacted_list: list = await self.get_reacts(prev_message.reactions)
+        member_names: set = set([member.display_name for member in reacted_list])
+        embed: discord.Embed = discord.Embed(title="**DOW3 Event time**",
+                              colour=discord.Colour.red())
+        event_author: discord.Member = channel.guild.get_member(author_id)
+        embed.set_author(name=event_author.display_name,
+                         icon_url=event_author.avatar_url)
+        embed.set_image(
+            url=self.image_urls['dow3'])
+        if member_names:
+            embed.add_field(name="Members that signed up",
+                            value=", ".join(member_names))
+        embed.add_field(name="Plan of action",
+                        value=f"{message}")
+        current_message: discord.Message = await channel.send(role.mention, embed=embed, allowed_mentions=discord.AllowedMentions(roles=True))
+        await prev_message.delete()
+        await asyncio.sleep(60)
+        await role.delete()
+        return await current_message.delete(delay=900)
+
+    @commands.Cog.listener()
+    async def on_ih_timer_complete(self, event):
+        """ On 'dow3' event timer complete. """
+        author_id, channel_id, message = event.args
+
+        try:
+            channel: discord.TextChannel = self.bot.get_channel(channel_id) or (await self.bot.fetch_channel(channel_id))
+        except discord.HTTPException:
+            return
+
+        message_id: int = event.kwargs.get('message_id')
+        prev_message: discord.Message = await channel.fetch_message(message_id)
+        role: discord.Role = channel.guild.get_role(event.kwargs.get('role_id'))
+        await role.edit(mentionable=True)
+        reacted_list: list = await self.get_reacts(prev_message.reactions)
+        member_names: set = set([member.display_name for member in reacted_list])
+        embed: discord.Embed = discord.Embed(title="**Iron Harvest Event time**",
+                              colour=discord.Colour.red())
+        event_author: discord.Member = channel.guild.get_member(author_id)
+        embed.set_author(name=event_author.display_name,
+                         icon_url=event_author.avatar_url)
+        embed.set_image(
+            url=self.image_urls['ih'])
+        if member_names:
+            embed.add_field(name="Members that signed up",
+                            value=", ".join(member_names))
+        embed.add_field(name="Plan of action",
+                        value=f"{message}")
+        current_message: discord.Message = await channel.send(role.mention, embed=embed, allowed_mentions=discord.AllowedMentions(roles=True))
+        await prev_message.delete()
+        await asyncio.sleep(60)
+        await role.delete()
+        return await current_message.delete(delay=900)
+
+    @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         """ Let's give event go-ers a role! """
         if not payload.guild_id:
@@ -665,6 +826,9 @@ class Specialist(commands.Cog):
                    OR event = 'bf2'
                    OR event = 'coh2'
                    OR event = 'swtor'
+                   OR event = 'gtfo'
+                   OR event = 'dow3'
+                   OR event = 'ih'
                 """
         records = await self.bot.pool.fetch(query)
         for record in records:
@@ -692,6 +856,8 @@ class Specialist(commands.Cog):
                    OR event = 'coh2'
                    OR event = 'swtor'
                    OR event = 'gtfo'
+                   OR event = 'dow3'
+                   OR event = 'ih'
                 """
         records = await self.bot.pool.fetch(query)
         for record in records:
